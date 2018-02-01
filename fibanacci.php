6161c68bf470540df4b85570f48ddcf9b9ddff36
LatypovAlexey <35660816+LatypovAlexey@users.noreply.github.com>
2018-02-01 20:07:13 +0500

    add

--- fibanacci.php
+++ fibanacci.php
@@ -1,24 +1,19 @@
 <?php
-function aaa($x) {
+$x = 13;
     $x1 = 1;
     $x2 = 1;
     while (true) {
         if ($x1 > $x) {
-            return false;
+            echo 'не входит';
+            break;
         }
         if ($x1 == $x) {
-            return true;
+            echo 'входит';
+            break;
         }
         $x3 = $x1;
         $x1 += $x2;
         $x2 = $x3;
     }
-}
-
-for ($i = 0; $i <= 50000; $i++) {
-    if (aaa($i)) {
-        echo $i . ';';
-    }
-}
 
 ?>
